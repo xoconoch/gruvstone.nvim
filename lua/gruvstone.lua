3d0b5d30aa3e8f9155a3e58ab3dafa8dfb01e4d0
xoconoch <xoconoch@cordovault.com>
2026-04-02 11:38:31 -0600

    update shit

--- lua/gruvstone.lua
+++ lua/gruvstone.lua
@@ -338,7 +338,7 @@ local function get_groups()
     EndOfBuffer                      = { link = "Punctuation" },
     Whitespace                       = { fg = colors.bg2 },
     -- UI elements
-    Directory                        = { fg = colors.red },
+    Directory                        = { link = "Error" },
     CursorLine                       = { bg = colors.bg1 },
     CursorColumn                     = { link = "CursorLine" },
     LineNr                           = { fg = colors.fg4 },
